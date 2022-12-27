51ecd5733885d127fbc7808db138bf42b2e5c8bc
onoot <101798777+onoot@users.noreply.github.com>
2022-12-27 22:39:33 +0700

    Update and rename src/components/ProductList/ProductList.jsx to Src/Компоненты/Список продуктов/ProductList.jsx

--- Src/Компоненты/Список продуктов/ProductList.jsx
+++ Src/Компоненты/Список продуктов/ProductList.jsx
@@ -5,14 +5,9 @@ import {useTelegram} from "../../hooks/useTelegram";
 import {useCallback, useEffect} from "react";
 
 const products = [
-    {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
-    {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
-    {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
-    {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
-    {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
-    {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+    {id: '1', title: 'Джинсы', price: 5000, img:'app-react//public/favicon.ico', description: 'Синего цвета, прямые'},
+    {id: '2', title: 'Куртка', price: 12000, img:'app-react//public/favicon.ico', description: 'Зеленого цвета, теплая'},
+
 ]
 
 const getTotalPrice = (items = []) => {
